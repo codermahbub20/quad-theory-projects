038068c1916fad3301a933fb3db9d064e36f568f
codermahbub20 <mkrefat5@gmail.com>
2024-01-06 01:51:58 +0600

    Popular item section implemented

--- src/Layouts/Pages/Home/Home/Home.jsx
+++ src/Layouts/Pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "../Banner/Banner";
+import Popular from "../Popular/Popular";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <div className="sm:w-full md:w-full lg:w-3/4 mx-auto">
+                <Popular></Popular>
+            </div>
         </div>
     );
 };
---
+++ src/Layouts/Pages/Home/Popular/Popular.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+
+
+
+const Popular = () => {
+    const [popularItem, setPopularItem] = useState([])
+
+    useEffect(() => {
+        fetch('http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10')
+            .then(res => res.json())
+            .then(data => {
+                setPopularItem(data.Items)
+            })
+    }, [])
+
+
+    return (
+        <div>
+
+            <div className=' flex justify-between mb-3'>
+                <div>
+                    <h1 className='text-2xl font-medium'>Popular</h1>
+                </div>
+                <div>
+                    <button className='btn'>Add More</button>
+                </div>
+            </div>
+
+            <Swiper
+                spaceBetween={10}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+                breakpoints={{
+                    200: {
+                        slidesPerView: 3, // show 3 slides per view on small screens
+                    },
+                    768: {
+                        slidesPerView: 4, // show 4 slides per view on medium screens
+                    },
+                    1024: {
+                        slidesPerView: 5, // show 5 slides per view on large screens
+                    },
+                }}
+            >
+                {
+                    popularItem?.map(item => <SwiperSlide key={item.Id}>
+                        <img className='h-[300px] w-[200px] rounded-xl' src={item?.ImageUrl} alt="" />
+                        <h1 className='text-xl text-center mb-10  mt-2'>{item?.Name}</h1>
+                    </SwiperSlide>)
+                }
+
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default Popular;
